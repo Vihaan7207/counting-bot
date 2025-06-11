0c287c07b16985013591de0e195270d372ff8f9d
Vihaan7207 <vmikki7207@gmail.com>
2025-06-11 17:36:06 -0400

    Find command JS files

--- index.js
+++ index.js
@@ -10,10 +10,26 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
+// Get every folder inside ./commands
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    
+    const commandsPath = path.join(foldersPath, folder);
+    // Get every JS file in folder
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        // Find module.exports
+        const command = require(filePath);
+        // Check all discord requirements are fulfilled
+        if ('data' in command && 'execute' in command) {
+            // Create a new item in the Collection with given name and command
+            client.commands.set(command.data.name, command);
+        }
+        else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
 }
 
 // When the client is ready, run this code (only once).
