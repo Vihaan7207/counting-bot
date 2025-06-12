eba6d7e6ebfdcd84c7fe452a49826f5969d0112b
Vihaan7207 <vmikki7207@gmail.com>
2025-06-11 21:54:13 -0400

    Moved event listeners to separate files

--- index.js
+++ index.js
@@ -1,68 +1,26 @@
-// Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
-// Get every folder inside ./commands
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    // Get every JS file in folder
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        // Find module.exports
-        const command = require(filePath);
-        // Check all discord requirements are fulfilled
-        if ('data' in command && 'execute' in command) {
-            // Create a new item in the Collection with given name and command
-            client.commands.set(command.data.name, command);
-        }
-        else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
-// Handler for all slash commands
-client.on(Events.InteractionCreate, async interaction => {
-    // Make sure the input is a slash command
-    if (!interaction.isChatInputCommand()) return;
-
-    // Find the command that the user is trying to run
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-    }
-});
-
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
-// Log in to Discord with your client's token
 client.login(token);
